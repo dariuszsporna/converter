e02996ded6b0abedd0fb69c74c01d36af675d507
dariuszsporna <47335340+dariuszsporna@users.noreply.github.com>
2021-05-27 16:37:08 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,54 @@
+const converter = document.querySelector('#converter');
+const result = document.querySelector('.result');
+const convBtn = document.querySelector('.conv');
+const resetBtn = document.querySelector('.reset');
+const changeBtn = document.querySelector('.change');
+const one = document.querySelector('.one');
+const two = document.querySelector('.two');
+let fahrenheit;
+let celsius;
+
+  const swap = () => {
+    if (one.innerText === '°C') {
+        one.innerText = '°F';
+        two.innerText = '°C'
+        result.innerText = '';
+    } else {
+        one.innerText ='°C';
+        two.innerText ='°F';
+        result.innerText = '';
+    }
+ };
+
+  const celToFahr = () => {
+    fahrenheit = converter.value * 1.8 + 32;
+    result.innerText = `${converter.value}°C to ${fahrenheit.toFixed(1)}°F`;
+    converter.value = '';
+   
+ };
+
+    const fahrToCel = () => {
+    celsius = (converter.value - 32) / 1.8;
+    result.innerText = `${converter.value}°F to ${celsius.toFixed(1)}°C`;
+    converter.value = '';
+    };
+
+    const conversion = () => {
+    if (converter.value !== ''){
+        if(one.innerText === '°C'){
+            celToFahr();
+            }
+        } else{
+            result.innerText = 'Musisz wpisać jakąś liczbę!'
+        }
+     };
+
+        const reset = () => {
+        converter.value = '';
+        result.innerText = '';
+};
+
+
+convBtn.addEventListener('click', celToFahr);
+changeBtn.addEventListener('click', swap);
+resetBtn.addEventListener('click', reset);
